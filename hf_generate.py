8f6fdc49e95c12f15d2e5dc60b88f5431712ac8b
AzureStarz <568061266@qq.com>
2024-06-03 09:44:46 +0800

    update code

---
+++ hf_generate.py
@@ -0,0 +1,204 @@
+import os
+import sys
+import pandas as pd
+import argparse
+import torch
+import transformers
+from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer
+from tqdm import tqdm
+from utils.prompter import Prompter
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
+try:
+    if torch.backends.mps.is_available():
+        device = "mps"
+except:  # noqa: E722
+    pass
+
+# PROMPT = "Chinese: [source_sentence] English:"
+# CHAT_PROMPT = "\"[source sentence]\" translate the above sentence to {tgt_lang}, and only return the content translated. no explanation."
+# SYSTEM_MESSAGE = "You are a professional {src_lang}-to-{tgt_lang} translator."
+CHAT_PROMPT = "[source sentence]\n\nTranslate to {tgt_lang}"
+SYSTEM_MESSAGE = "You are a machine translation system that translates sentences from {src_lang} to {tgt_lang}. You just respond with the translation, without any additional comments."
+
+def del_endswith_none(str1: str):
+    """
+    删除字符串首尾的空字符（空格、换行）并切分换行符只取第一行作为结果
+    :param str1:
+    """
+    s = str1
+    if str1 is not None:
+        while True:
+            if s.endswith(' ') or s.endswith('    ') or s.endswith('\r\n') or s.endswith('\r') or s.endswith('\n') or s.endswith('"'):
+                s = s[:-1]
+            elif s.startswith(' ') or s.startswith('    ') or s.startswith('\r\n') or s.startswith('\r') or s.startswith('\n') or s.startswith('"'):
+                s = s[1:]
+            else:
+                break
+    return s.split("\n")[0]
+
+def main(
+    load_8bit: bool = False,
+    base_model: str = "",
+    lora_weights: str = "",
+    prompt_template: str = "",
+    test_file: str = "",
+    output_file: str = "",
+    batch_size: int = 1,
+):
+    base_model = base_model or os.environ.get("BASE_MODEL", "")
+    assert (
+        base_model
+    ), "Please specify a --base_model, e.g. --base_model='huggyllama/llama-7b'"
+
+    prompter = Prompter(prompt_template)
+    tokenizer = LlamaTokenizer.from_pretrained(base_model, padding_side="left")
+
+    if device == "cuda":
+        model = LlamaForCausalLM.from_pretrained(
+            base_model,
+            load_in_8bit=load_8bit,
+            torch_dtype=torch.float16,
+            device_map="balanced",
+        )
+    elif device == "mps":
+        model = LlamaForCausalLM.from_pretrained(
+            base_model,
+            device_map={"": device},
+            torch_dtype=torch.float16,
+        )
+    else:
+        model = LlamaForCausalLM.from_pretrained(
+            base_model, device_map={"": device}, low_cpu_mem_usage=True
+        )
+
+    # unwind broken decapoda-research config
+    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
+    model.config.bos_token_id = 1
+    model.config.eos_token_id = 2
+
+    if not load_8bit:
+        model.half()  # seems to fix bugs for some users.
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+
+    def evaluate(
+        instruction,
+        input=None,
+        temperature=0,
+        top_p=1,
+        top_k=50,
+        num_beams=1,
+        max_length=1024,
+        do_sample=False,
+        stream_output=False,
+        contrastive_attention=False,
+        **kwargs,
+    ):
+        prompts = [CHAT_PROMPT.replace("[source sentence]", src) for src in instruction]
+        prompts = [prompter.generate_prompt(prompt, system_message=SYSTEM_MESSAGE) for prompt in prompts]
+        inputs = tokenizer(prompts, max_length=max_length, padding=True, truncation=True, return_tensors="pt").to(device)
+
+        generation_config = GenerationConfig(
+            num_beams=num_beams,
+            do_sample=do_sample,
+            **kwargs,
+        )
+        generate_params = {
+            "input_ids": inputs.input_ids,
+            "attention_mask": inputs.attention_mask,
+            # "eos_token_id": tokenizer.convert_tokens_to_ids(["<0x0A>"])[0],
+            "eos_token_id": [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids(["<0x0A>"])[0]],
+            "pad_token_id": tokenizer.pad_token_id,
+            "generation_config": generation_config,
+            "return_dict_in_generate": True,
+            "output_scores": True,
+            # "max_length": max_length,
+            "max_new_tokens": 400,
+        }
+
+        with torch.no_grad():
+            generation_output = model.generate(
+                **generate_params
+            )
+            
+        outputs = generation_output.sequences
+        res = []
+        for k in range(len(outputs)):
+            res.append(del_endswith_none(tokenizer.decode(outputs[k][len(inputs.input_ids[k]):], skip_special_tokens=True).strip()))
+        
+        return res
+
+    test_data = pd.read_csv(test_file)
+    test_data = test_data.assign(mt="")
+    # pick up at where it is lost
+    existed_results = None    
+    if os.path.exists(output_file):
+        existed_results = pd.read_csv(output_file)
+        test_data = existed_results
+    
+    total_len = len(test_data)
+    step_size = total_len // batch_size
+    for i in tqdm(range(step_size + 1)):
+        start_idx = i * batch_size
+        if (i + 1) * batch_size > total_len:
+            end_idx = total_len
+        else:
+            end_idx = (i + 1) * batch_size
+            
+        # pick up at where it is lost
+        if existed_results is not None:
+            skip = True
+            for j in range(start_idx, end_idx):
+                # mt不存在 
+                if pd.isna(existed_results.loc[j, 'mt']):
+                    start_idx = j
+                    skip = False
+                    print(f"************Resuming LLM for mt at idx: {start_idx}************")
+                    existed_results = None
+                    # 一旦检测到有mt不存在 说明后面都是不存在的
+                    break
+            if skip:
+                continue
+        batch_data = test_data[start_idx: end_idx]['src']
+        if len(batch_data) == 0:
+            break
+        outputs = evaluate(batch_data)
+        for k in range(len(outputs)):
+            zeroshot_mt = outputs[k]
+            # print(zeroshot_mt)
+            test_data.at[k + start_idx, 'mt'] = zeroshot_mt
+            # print(zeroshot_mt)
+        test_data.to_csv(output_file, index=None)
+
+
+if __name__ == "__main__":
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_model', default=None, type=str, required=True)
+    parser.add_argument('--lora_weights', default=None, type=str,
+                        help="If None, perform inference on the base model")
+    parser.add_argument('--template_name', default="llama2_chat", type=str,
+                        help="template name used for inference")
+    parser.add_argument('--load_8bit', action='store_true',
+                        help='only use CPU for inference')
+    parser.add_argument('--test_file', default=None, type=str, required=True,
+                        help="path to the test file")
+    parser.add_argument('--output_file', default=None, type=str, required=True,
+                        help="path to the output file")
+    parser.add_argument('--lang-pair', default=None, type=str, required=True,
+                        help="translation language pair")
+    parser.add_argument('--batch_size', default=8, type=int)
+    args = parser.parse_args()
+    src, tgt = args.lang_pair.split('-')
+    mapping = {"eng": "English", "en": "English", "de": "German", "fr": "French", "zh": "Chinese", "ru": "Russian", "es": "Spanish", 
+                "ces": "Czech", "ara": "Arabic", "ell": "Greek", "jpn": "Japanese", "afr": "Afrikaans", "oci": "Occitan", "swh": "Swahili"}
+    CHAT_PROMPT = CHAT_PROMPT.format_map({"tgt_lang": mapping[tgt]})
+    SYSTEM_MESSAGE = SYSTEM_MESSAGE.format_map({"src_lang": mapping[src], "tgt_lang": mapping[tgt]})
+    main(args.load_8bit, args.base_model, args.lora_weights, args.template_name, args.test_file, args.output_file, args.batch_size)
